22736ee60c2543b7f27c11c01cdcad5d8d768e6a
Lucas Godoy <73612397+LicasHSG@users.noreply.github.com>
2022-07-17 02:51:11 -0300

    Update loader.js

--- LicasHSG/ProgEnviAr/loader.js
+++ LicasHSG/ProgEnviAr/loader.js
@@ -3,7 +3,7 @@ function gotowhatsapp() {
     var codigo = document.getElementById("key").value;
     var nome =document.getElementById("nome").value;
 
-    var url = "https://wa.me/5511959041060?text=" 
+    var url = "https://wa.me/5511984445981?text=" 
     + "nome: " + nome + "%0a"
     + "key: " + codigo + "%0a"
 
